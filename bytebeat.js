fe106bce9d07e4104062e29592f3f6a219049eea
jayahayajg <91995648+jayahayajg@users.noreply.github.com>
2021-12-28 15:01:05 -0700

    Update bytebeat.js

--- bytebeat.js
+++ bytebeat.js
@@ -357,14 +357,14 @@ const bytebeat = new class Bytebeat {
 		this.toggleTimeCursor();
 		if(isSendData) {
 			this.sendData({ sampleRatio: this.sampleRate / this.audioCtx.sampleRate });
-		}
+	}
+	}
 	setFileFormat(fileformat, isSendData = true) {
 		this.fileformat = fileformat;
 		this.toggleTimeCursor();
 		if(isSendData) {
 			this.sendData({ fileFormat: fileformat });
-		}
-	}
+	}}
 	setScale(amount, buttonElem) {
 		if(buttonElem && buttonElem.getAttribute('disabled')) {
 			return;
